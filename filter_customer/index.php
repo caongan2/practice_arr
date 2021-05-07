94e35fa2846b75c73b3f6ec9ffbe1e53e6743f1b
caongan <caothuongngan@gmail.com>
2021-05-07 14:27:18 +0700

    filter_list

---
+++ filter_customer/index.php
@@ -0,0 +1,110 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        th, td {
+            padding: 8px;
+            text-align: left;
+            border-bottom: 1px solid red;
+        }
+    </style>
+</head>
+<body>
+<form action="" method="get">
+    Chon ngay sinh tu: <input type="date" name="from" id="from" placeholder="yyyy/mm/dd" value=" ">
+    Den: <input type="date" name="to" id="to" placeholder="yyyy/mm/dd" value=" ">
+    <input type="submit" id="submit" value="Filter">
+</form>
+
+</body>
+</html>
+<?php
+$customerList = [
+    "1" => [
+        "name" => "Mai Văn Hoàn",
+        "day_of_birth" => "1983/08/20",
+        "address" => "Hà Nội",
+        "profile" => "images/img1.jpg"],
+    "2" => [
+        "name" => "Nguyễn Văn Nam",
+        "day_of_birth" => "1983/08/21",
+        "address" => "Bắc Giang",
+        "profile" => "images/img2.jpg"],
+    "3" => [
+        "name" => "Nguyễn Thái Hòa",
+        "day_of_birth" => "1983/08/22",
+        "address" => "Nam Định",
+        "profile" => "images/img3.jpg"],
+    "4" => [
+        "name" => "Trần Đăng Khoa",
+        "day_of_birth" => "1983/08/17",
+        "address" => "Hà Tây",
+        "profile" => "images/img4.jpg"],
+    "5" => [
+        "name" => "Nguyễn Đình Thi",
+        "day_of_birth" => "1983/08/19",
+        "address" => "Hà Nội",
+        "profile" => "images/img5.jpg"]
+];
+$fromDate = null;
+$toDate = null;
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $fromDate = $_REQUEST["from"];
+    $toDate = $_REQUEST["to"];
+}
+$filteredCustomers = searchByDate($customerList, $fromDate, $toDate);
+
+function searchByDate($customer, $fromDate, $toDate) {
+    if (empty($fromDate) || empty($toDate)) {
+        return $customer;
+    }
+
+       $filteredCustomers = [];
+    foreach ($customer as $customer) {
+        if (strtotime($customer['day_of_birth']) < strtotime($fromDate))
+            continue;
+        if (strtotime($customer['day_of_birth']) > strtotime($toDate))
+            continue;
+        $filteredCustomers[] = $customer;
+    }
+    return $filteredCustomers;
+}
+
+
+?>
+
+
+
+
+<table border="0">
+    <caption><h2>Danh sach khach hang</h2></caption>
+    <tr>
+        <th>STT</th>
+        <th>Ten</th>
+        <th>Ngay Sinh</th>
+        <th>Dia chi</th>
+        <th>Anh</th>
+    </tr>
+        <?php foreach ($filteredCustomers as $index => $customer): ?>
+        <tr>
+            <td><?php echo $index + 1; ?></td>
+            <td><?php echo $customer['name']; ?></td>
+            <td><?php echo $customer['day_of_birth']; ?></td>
+            <td><?php echo $customer['address']; ?></td>
+            <td>
+                <div class="profile"><img src="<?php echo $customer['profile']; ?>"/></div>
+            </td>
+        </tr>
+    <?php endforeach; ?>
+</table>
